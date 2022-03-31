9398a7b1ab6ac9e386f0031acc9d17c44a88aff1
Odis Clemons <odis.clemons@gmail.com>
2022-03-31 15:01:33 -0400

    refactored generateChars function into a ternary to handle all types of chars

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const lowerCase = [97, 123];
 //prettier-ignore
 const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(" , ")", "_", "+", ")"];
 
+// by default our password length and number of each character to get
 var passwordLength = 8;
-var numOfChars = 2;
+var numOfEach = 2;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 async function writePassword() {
+  // wait for results of generatePassword
   var password = await generatePassword();
   var passwordText = document.querySelector("#password");
-  console.log(password, password.length);
+  // console.log(password, password.length);
   passwordText.value = password;
 }
 
@@ -28,17 +30,18 @@ async function generatePassword() {
   let tempPassword = "";
 
   // find out how many characters their password should be and then divide each type of allowed character evenly
-  // keep asking them until you get a valid answer also
+  // also, keep asking them until you get a valid answer.  default to 8 chars
   do {
     passwordLength = parseInt(
       prompt(
-        "How long should your password be? (between 8 and 128 characters only)"
+        "How long should your password be? (between 8 and 128 characters only)",
+        8
       )
     );
   } while (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
 
+  //prompt ask for each type of character
   let useUpperCase = confirm("Would you like uppercase characters?");
-
   let useLowerCase = confirm("How about lowercase ones?");
   let useNumbers = confirm("Numbers in your password?");
   let useSpecialChars = confirm("Would you like special characters?");
@@ -46,55 +49,46 @@ async function generatePassword() {
   // I just learned if you simply add those true false values js converts them to integers
   // So lets add them and divide the password length by the total number of different types of characters
   // That way we generate an evenly distributed amount of each in the password
-  numOfChars = Math.floor(
+  numOfEach = Math.floor(
     passwordLength /
       (useUpperCase + useLowerCase + useNumbers + useSpecialChars)
   );
-  console.log(numOfChars);
+  console.log(numOfEach);
 
-  if (useUpperCase) tempPassword += await generateChars(upperCase, numOfChars);
-  console.log(tempPassword);
-  if (useLowerCase) tempPassword += await generateChars(lowerCase, numOfChars);
-  console.log(tempPassword);
-  if (useNumbers) tempPassword += await generateChars(numbers, numOfChars);
-  console.log(tempPassword);
-  if (useSpecialChars)
-    tempPassword += await generateChars(specialChars, numOfChars);
-  console.log(tempPassword);
+  if (useUpperCase) tempPassword += await generateChars(upperCase);
+  if (useLowerCase) tempPassword += await generateChars(lowerCase);
+  if (useNumbers) tempPassword += await generateChars(numbers);
+  if (useSpecialChars) tempPassword += await generateChars(specialChars);
   tempPassword = await addPadding(tempPassword);
   return tempPassword;
 }
 
-function generateChars(typeOfChar, numOfChars) {
+function generateChars(typeOfChar) {
   return new Promise((res, rej) => {
     let tempPassword = "";
     let i = 0;
-    if (typeOfChar.length === 2) {
-      let min = typeOfChar[0];
-      let max = typeOfChar[1];
-      do {
-        i++;
+    let min = typeOfChar[0];
+    let max = typeOfChar[1];
 
-        tempPassword = tempPassword + String.fromCharCode(randomNum(min, max));
-        if (i === numOfChars) res(tempPassword);
-      } while (i < numOfChars + 1);
-    } else {
-      if (numOfChars)
-        do {
-          i++;
-          tempPassword =
-            tempPassword + specialChars[randomNum(0, specialChars.length - 1)];
-          if (i === numOfChars) res(tempPassword);
-        } while (i < numOfChars + 1);
-    }
+    do {
+      i++;
+      // if typeOfChar length is 2, it is either upperCase, lowerCase, or a number
+      // otherwise it is the much longer specialChars array
+      tempPassword =
+        typeOfChar.length === 2
+          ? tempPassword + String.fromCharCode(randomNum(min, max))
+          : tempPassword + specialChars[randomNum(0, specialChars.length - 1)];
+      if (i === numOfEach) res(tempPassword);
+    } while (i < numOfEach + 1);
   });
 }
 
+// little function from stackOverflow to generate random number between min and max values
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// if the passwordLength isnt evenely divisibly by numOfChars, we wont generate the amount of characters
+// if the passwordLength isnt evenely divisibly by numOfEach, we wont generate the amount of characters
 // the user specified.  The way i'll solve that is by adding random characters until the length is correct
 function addPadding(tempPassword) {
   return new Promise((res, rej) => {
@@ -107,12 +101,12 @@ function addPadding(tempPassword) {
     let difference = passwordLength - tempPassword.length;
     let min = lowerCase[0];
     let max = lowerCase[1];
-    console.log(passwordLength, tempPassword.length, difference);
+
     //loop and generate random lowercase chars
     do {
       i++;
       padding += String.fromCharCode(randomNum(min, max));
-      console.log("padding:", padding);
+      // when we reach the goal, resolve the promise
       if (i === difference) res(tempPassword + padding);
     } while (i < difference + 1);
   });
